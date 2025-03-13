d7b7690fb2c8981d4672e472ba131cd33c46a9ba
madiha-m <madiha.muhammad.dev@gmail.com>
2025-03-13 19:54:50 +0500

    feat(python): add PYTHON_SANDBOX directory with variables.py for practice

---
+++ PYTHON_SANDBOX/variables.py
@@ -0,0 +1,15 @@
+long_str = '''use triple quote to format a long string, and this str is enclosed in it.'''
+print(long_str)
+
+print('Get 1st char from string, while we use 0 index, and consider string as array: ',
+      long_str[0])
+print('Get last char from string, while we use -1 index, and consider string as array: ',
+      long_str[-1])
+
+print('slice the string: ', long_str[0:5])
+
+first_part = long_str[0:10]
+last_part = long_str[-1:10]
+full = f"First Part: {first_part}. Last Part: {last_part}"
+
+print(full)
